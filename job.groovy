a164467211d656d59048178b9792cac1193590b6
devopsop <36001380+devopsop@users.noreply.github.com>
2019-01-23 16:32:48 -0500

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -33,11 +33,11 @@ def getUserPassword = { username ->
 def username = "wadacdci"
 def pass = java.net.URLEncoder.encode(getUserPassword(username), "UTF-8")
 def sout = new StringBuilder(), serr = new StringBuilder()
-def proc = ['/bin/bash', '-c', "git ls-remote --tags https://\$username:\$pass@bitbucket.wada-ama.org/scm/adams/adams.git builds/* | awk '\\\$2 !~ /\\\\^\\\\{\\\\}/ && sub(\\\"refs/tags/builds/\\",\\"\\",\\\$2) {print \\\$2}'"].execute()
+def proc = ['/bin/bash', '-c', "git ls-remote --tags https://\\$username:\\$pass@bitbucket.wada-ama.org/scm/adams/adams.git builds/* | awk '\\\\\\$2 !~ /\\\\\\\\^\\\\\\\\{\\\\\\\\}/ && sub(\\\\\\"refs/tags/builds/\\\\",\\\\"\\\\",\\\\\\$2) {print \\\\\\$2}'"].execute()
 proc.consumeProcessOutput(sout, serr)
 proc.waitForOrKill(1000)
-println("\$serr");
-return "\$sout".tokenize().reverse()
+println("\\$serr");
+return "\\$sout".tokenize().reverse()
 """)
                     }
                 }
@@ -60,8 +60,8 @@ echo deploying in CGI
 echo JIRA_ISSUES=$JIRA_TICKETS > env.properties
 echo ADAMS_VERSION=$DEPLOY_VERSION >> env.properties
 #echo mobileDeployPath=mobile-server-$mobileVersion.jar >> env.properties
-sed  -e "s/\(JIRA_ISSUES=\)/\1issueKey=/" -e "s/ \(ADAMS-[0-9]\{4\}\)/ or issuekey=\1/g" -i env.properties
-sed -e "s/\(^ADAMS_VERSION=[0-9.]*-[A-Z]*\)-[0-9]*/\1/g" -i env.properties
+sed  -e "s/\\(JIRA_ISSUES=\\)/\\1issueKey=/" -e "s/ \\(ADAMS-[0-9]\\{4\}\\)/ or issuekey=\\1/g" -i env.properties
+sed -e "s/\\(^ADAMS_VERSION=[0-9.]*-[A-Z]*\\)-[0-9]*/\\1/g" -i env.properties
         ''')
         environmentVariables {
             propertiesFile('$WORKSPACE/env.properties')
