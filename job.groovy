378bfd51cca0d37bc42e646b27c231e85ab18fda
devopsop <36001380+devopsop@users.noreply.github.com>
2019-01-23 13:39:39 -0500

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -16,7 +16,28 @@ def curJob = job('ADAMS-PROMOTIONS-DEV-DEVOPS') {
                         authentication('bitbucket_public_key')
                         outputFile('build$ADAMSBuildNumber.txt')                        
                     }
-                    groovyCommand('Hi')       
+                    groovyScript {
+                        sandbox(false)
+                        script("""
+import groovy.json.JsonSlurper
+import hudson.EnvVars
+import hudson.model.Environment
+def buildn= build.buildVariableResolver.resolve("ADAMSBuildNumber")
+def thr = Thread.currentThread();
+def currentBuild = thr?.executable;
+def workspace = currentBuild.getModuleRoot().absolutize().toString();
+println workspace
+def inputFile = new File(workspace + "/build" + buildn + ".txt")
+def InputJSON = new JsonSlurper().parseText(inputFile.text)
+def vardeploypath = (InputJSON =~ /deployPath:\\s(.+)/)[0][1]
+def varDEPLOY_VERSION = (InputJSON =~ /DEPLOY_VERSION:\\s(.+)/)[0][1]
+def varsqlVersion = (InputJSON =~ /sqlVersion:\\s(.+)/)[0][1]
+def varmobileVersion = (InputJSON =~ /mobileVersion:\\s(.+)/)[0][1]
+def varmobileDeployPath = (InputJSON =~ /mobileDeployPath:\\s(.+)/)[0][1]
+def build = Thread.currentThread().executable
+def vars = [deployPath: vardeploypath ,DEPLOY_VERSION: varDEPLOY_VERSION ,sqlVersion: varsqlVersion ,mobileVersion: varmobileVersion,mobileDeployPath: varmobileDeployPath]
+build.environments.add(0, Environment.create(new EnvVars(vars)))
+""")      
                     downstreamParameterized {
                         trigger('ADAMS_DEPLOY_CGI_MERGED') {
                             parameters {
