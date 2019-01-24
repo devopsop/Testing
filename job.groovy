173fda4b33d7f34949a45081205fc34e0c87e196
devopsop <36001380+devopsop@users.noreply.github.com>
2019-01-24 13:35:31 -0500

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,54 +1,56 @@
-def curJob = job('ADAMS_DEPLOY_CGI_MERGED') {
+def curJob = job('ADAMS-PROMOTIONS-DEV-DEVOPS') {
+    properties {
+        promotions {
+            promotion {
+                name('ADAMS Promotion DEV')
+                icon('Green star')
+                conditions {
+                    manual('ACL_CDCI_deploy_CH,ACL_CDCI_deploy_QA,ACL_CDCI_deploy_ADMIN')
+                }
+                actions {
+                    httpRequest {
+                        url('https://wada-ama.atlassian.net/rest/api/2/search?jql=id=${JIRA_KEY}&fields=description')
+                        httpMode('GET')
+                        acceptType('APPLICATION_JSON')
+                        contentType('APPLICATION_JSON')
+                        authentication('bitbucket_public_key')
+                        outputFile('build$ADAMSBuildNumber.txt')                        
+                    }    
+                    shell('''
+deployPath=$(grep -Po 'deployPath:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
+DEPLOY_VERSION=$(grep -Po 'DEPLOY_VERSION:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
+sqlVersion=$(grep -Po 'sqlVersion:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
+mobileVersion=$(grep -Po 'mobileVersion:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
+mobileDeployPath=$(grep -Po 'mobileDeployPath:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
+
+echo "deployPath=${deployPath}" > varfile
+echo "DEPLOY_VERSION=${DEPLOY_VERSION}" >> varfile
+echo "sqlVersion=${sqlVersion}" >> varfile
+echo "mobileVersion=${mobileVersion}" >> varfile
+echo "mobileDeployPath=${mobileDeployPath}" >> varfile
+                    ''')
+                    environmentVariables {
+                        propertiesFile('varfile')
+                    }
+                    downstreamParameterized {
+                        trigger('ADAMS_DEPLOY_CGI_MERGED') {
+                            parameters {
+                                predefinedProp('DEPLOY_VERSION', '$DEPLOY_VERSION')
+                                predefinedProp('deployPath', '$deployPath')
+                                predefinedProp('sqlVersion', '$sqlVersion')
+                                predefinedProp('mobileVersion', '$mobileVersion')
+                                predefinedProp('mobileDeployPath', '$mobileDeployPath')
+                                predefinedProp('JIRA_KEY', '${JIRA_KEY}')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     parameters {
-        stringParam('DEPLOY_VERSION', '6.5.0-SNAPSHOT', 'Adams version to be deployed')
-        stringParam('deployPath', 'adams.ear', 'path of the adams.ear')
-        stringParam('mobileVersion', '3.0.3.FINAL', 'Mobile Version to deploy')
-        stringParam('mobileDeployPath', 'mobile-server-3.0.3-FINAL.jar', 'Mobile Version to deploy')
-        stringParam('JIRA_TICKETS', 'none', 'space separated list of Jira tickets related to this build')
-        stringParam('smssuppressValue', 'false', 'sms supression ')
-        stringParam('smtpsuppressValue', 'false', 'smtp supression')
-        stringParam('JIRA_KEY', 'none', 'JIRA Issue created for the build')
-
-    }
-
-    steps {
-        shell('''
-echo deploying in CGI
-
-echo JIRA_ISSUES=$JIRA_TICKETS > env.properties
-echo ADAMS_VERSION=$DEPLOY_VERSION >> env.properties
-#echo mobileDeployPath=mobile-server-$mobileVersion.jar >> env.properties
-sed  -e "s/\\(JIRA_ISSUES=\\)/\\1issueKey=/" -e "s/ \\(ADAMS-[0-9]\\{4\\}\\)/ or issuekey=\\1/g" -i env.properties
-sed -e "s/\\(^ADAMS_VERSION=[0-9.]*-[A-Z]*\\)-[0-9]*/\\1/g" -i env.properties
-        ''')
-        environmentVariables {
-            propertiesFile('$WORKSPACE/env.properties')
-        }
-        shell('echo $ADAMS_VERSION')
-        shell('''
-knife environment show adams_cgi  -c /opt/cdci/chef/knife-test-new.rb  -F json | jq .cookbook_versions
-sh /opt/databags/create-databag-test-cgi.sh "/org/wada/adams/adams-ear/$ADAMS_VERSION/$deployPath" "$deployPath" "/opt/databags/dev-data-bag.json" "$sqlVersion" "$mobileVersion" "$mobileDeployPath"
-knife data bag from file cdci  /opt/databags/dev-data-bag.json --secret-file /opt/cdci/chef/secret_key -c /opt/cdci/chef/knife-test-new.rb
-sh /opt/databags/create-databag-qaconfigs-test.sh "$smssuppressValue" "$smtpsuppressValue" "/opt/databags/dev-data-bag.json"
-knife data bag from file jboss7_cgi  /opt/databags/dev-data-bag.json --secret-file /opt/cdci/chef/secret_key -c /opt/cdci/chef/knife-test-new.rb
-knife job start chef-client WVTEMADA09.test.wada-ama.org -c /opt/cdci/chef/knife-test-new.rb --capture
-        ''')
-        progressJiraIssues {
-            jqlSearch('id=${JIRA_KEY}')
-            workflowActionName('TO DO')
-        }
-        progressJiraIssues {
-            jqlSearch('id=${JIRA_KEY}')
-            workflowActionName('TO DO')
-            comment('Deployed in CGI')
-        }
-    }
-
-    publishers {
-        retryBuild {
-            retryLimit(1)
-            fixedDelay(10)
-        }
+        stringParam('ADAMSBuildNumber', null, 'Displayed Build Number')
+        stringParam('JIRA_KEY', 'none', 'JIRA Key for Build')
     }
 }
