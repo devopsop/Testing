c7da3d9099471ac1ff61d39941ad1abdfda5c25a
devopsop <36001380+devopsop@users.noreply.github.com>
2019-01-24 13:39:13 -0500

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -16,22 +16,7 @@ def curJob = job('ADAMS-PROMOTIONS-DEV-DEVOPS') {
                         authentication('bitbucket_public_key')
                         outputFile('build$ADAMSBuildNumber.txt')                        
                     }    
-                    shell('''
-deployPath=$(grep -Po 'deployPath:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
-DEPLOY_VERSION=$(grep -Po 'DEPLOY_VERSION:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
-sqlVersion=$(grep -Po 'sqlVersion:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
-mobileVersion=$(grep -Po 'mobileVersion:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
-mobileDeployPath=$(grep -Po 'mobileDeployPath:\\s.*?\\\\n' jira.json | cut -d' ' -f2 | cut -d'\\' -f1)
-
-echo "deployPath=${deployPath}" > varfile
-echo "DEPLOY_VERSION=${DEPLOY_VERSION}" >> varfile
-echo "sqlVersion=${sqlVersion}" >> varfile
-echo "mobileVersion=${mobileVersion}" >> varfile
-echo "mobileDeployPath=${mobileDeployPath}" >> varfile
-                    ''')
-                    //environmentVariables {
-                    //    propertiesFile('varfile')
-                    //}
+ 
                     downstreamParameterized {
                         trigger('ADAMS_DEPLOY_CGI_MERGED') {
                             parameters {
